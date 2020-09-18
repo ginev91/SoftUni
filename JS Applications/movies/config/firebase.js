9d3fc3c8e240c922493a8d1864df93d783e388a4
ginev91 <48861476+ginev91@users.noreply.github.com>
2020-09-18 10:08:21 +0300

    Create firebase.js

---
+++ JS Applications/movies/config/firebase.js
@@ -0,0 +1,13 @@
+ 
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+  apiKey: "AIzaSyCsJLetr-fNUJR3yl7tK9JpkoKteghUC1E",
+  authDomain: "exam-10bec.firebaseapp.com",
+  databaseURL: "https://exam-10bec.firebaseio.com",
+  projectId: "exam-10bec",
+  storageBucket: "exam-10bec.appspot.com",
+  messagingSenderId: "332179354847",
+  appId: "1:332179354847:web:78a2d9c31e2a729b71e56a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
